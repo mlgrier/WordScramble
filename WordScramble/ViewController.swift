5612ea9e062a36086269991cb34c31916b98af57
Marco Grier <mlgrier@gmail.com>
2017-04-26 19:48:29 -0500

    Turned file into an array

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import GameplayKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Code below took the file and made turned the file into an array
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                
+                allWords = startWords.components(separatedBy: "\n")
+                
+            } else {
+                
+                allWords = ["silkworm"]
+                
+            }
+        }
+    }
+    
+    func startGame() {
+        
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
