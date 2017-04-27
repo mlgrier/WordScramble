9fc71c1d63c1def347e049b2154dd4f77e39f3ea
Marco Grier <mlgrier@gmail.com>
2017-04-26 21:09:05 -0500

    Added spell checker

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -97,19 +97,39 @@ class ViewController: UITableViewController {
     
     func isPossible(word: String) -> Bool {
         
+        var tempWord = title!.lowercased()
+        
+        for letter in word.characters {
+            
+            if let pos = tempWord.range(of: String(letter)) {
+                
+                tempWord.remove(at: pos.lowerBound)
+                
+            } else {
+            
+                return false
+            }
+        }
+        
         return true
         
     }
     
     func isOriginal(word: String) -> Bool {
         
-        return true
+        return !usedWords.contains(word)
         
     }
     
+    
+    // The func is used to check and see if a word was spelled right
     func isReal(word: String) -> Bool {
         
-        return true
+        let checker = UITextChecker()//UITextChecker is used to check spelling of word
+        let range = NSMakeRange(0, word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
         
     }
 
