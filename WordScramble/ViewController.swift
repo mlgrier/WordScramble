682614ecc5b980952bb15671d88730be96bd847c
Marco Grier <mlgrier@gmail.com>
2017-04-26 20:53:14 -0500

    Added code to lower case the words and add user entered words to the table view

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         //Code below took the file and made turned the file into an array
         
         if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
@@ -31,6 +33,8 @@ class ViewController: UITableViewController {
                 
             }
         }
+        
+        startGame()
     }
     
     func startGame() {
@@ -41,6 +45,73 @@ class ViewController: UITableViewController {
         tableView.reloadData()
         
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        
+        cell.textLabel?.text = usedWords[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func promptForAnswer() {
+        
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] _ in
+            
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+    
+    func submit(answer: String) {
+        
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        
+        return true
+        
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        
+        return true
+        
+    }
+    
+    func isReal(word: String) -> Bool {
+        
+        return true
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -49,4 +120,13 @@ class ViewController: UITableViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
 
